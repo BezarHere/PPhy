0be7b265a4e4ee6456e741c2b4c625256becc369
botatodev <botatodev@gmail.com>
2024-02-21 00:55:50 +0300

    implemented the batcher

--- PlugPhy/src/PlugPhy.cpp
+++ PlugPhy/src/PlugPhy.cpp
@@ -9,9 +9,75 @@ template Object3D;
 
 namespace pphy
 {
-	template<typename _OBJ>
-	TSpace<_OBJ>::TSpace()
+	namespace batchers
 	{
+		static constexpr size_t BatchResultInitCapacity = 1u << 8;
+		static constexpr size_t ObjectBatchInitCapacity = 1u << 8;
+
+		template<typename _OBJ>
+		TBoundsBatcher<_OBJ>::TBoundsBatcher() : m_results{} {
+			m_results.reserve( BatchResultInitCapacity );
+			for (ObjectBatch &batch : m_results)
+			{
+				batch.reserve( ObjectBatchInitCapacity );
+			}
+		}
+
+		template<typename _OBJ>
+		void TBoundsBatcher<_OBJ>::invalidate() {
+			m_dirty = true;
+		}
+
+		template<typename _OBJ>
+		void TBoundsBatcher<_OBJ>::rebuild( const std::vector<object_type> &objects ) {
+			m_dirty = false;
+
+			m_results.clear();
+
+			frame_type *const frames = new frame_type[ objects.size() ];
+			frame_type *const group_frames = new frame_type[ objects.size() ];
+
+			for (index_t i = 0; i < objects.size(); i++)
+			{
+				const object_type &obj = objects[ i ];
+				frames[ i ] = obj.get_frame().expanded( m_expand_margin );
+
+				bool found_group = false;
+				// single object's aabb can intersect multiple group frames
+				for (index_t group_index = 0; group_index < m_results.size(); group_index++)
+				{
+					if (frames[ i ].intersects( group_frames[ group_index ] ))
+					{
+						group_frames[ group_index ].encase( frames[ i ] );
+						m_results[ group_index ].push_back( i );
+						found_group = true;
+					}
+				}
+
+				if (!found_group)
+				{
+					m_results.emplace_back();
+					m_results.back().push_back( i );
+					group_frames[ m_results.size() - 1 ] = frames[ i ];
+				}
+
+			}
+
+			delete[] frames;
+			delete[] group_frames;
+		}
+	}
+
+
+	template<typename _OBJ>
+	TSpace<_OBJ>::TSpace() {
+	}
+
+	template<typename _OBJ>
+	void TSpace<_OBJ>::update( real_t deltatime ) {
+		m_batcher.try_rebuild( m_objects );
+		const BatchResult &b = m_batcher.get_results();
+
 	}
 
 	Shape2D::ShapeUnion2D::~ShapeUnion2D() {
