0501c293bdbd484aa0fc8109acd92547b4d1c881
botatodev <botatodev@gmail.com>
2024-02-21 21:52:12 +0300

    small change to demo

--- demo/src/main.cpp
+++ demo/src/main.cpp
@@ -3,7 +3,7 @@
 
 int main() {
 	pphy::Space2D space{};
-	pphy::Object2D object{ pphy::ObjectType::Static, pphy::ShapeType2D::None };
+	pphy::Object2D object{ pphy::ObjectType::Static };
 	space.add_object( object );
 	space.update( 1. / 60. );
 }
