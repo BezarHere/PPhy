d2c8d0d725b476516520a2189cd991af1cf88956
botatodev <botatodev@gmail.com>
2024-02-21 21:32:08 +0300

    demo update

--- demo/src/main.cpp
+++ demo/src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
-
+#include <PPhy.h>
 
 int main() {
-	
+	pphy::Space2D space{};
+	pphy::Object2D object{ pphy::ObjectType::Static, pphy::ShapeType2D::None };
+	space.add_object( object );
+	space.update( 1. / 60. );
 }
